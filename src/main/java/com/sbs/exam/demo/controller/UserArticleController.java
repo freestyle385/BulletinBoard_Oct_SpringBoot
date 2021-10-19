c408f4347ad28ebc2305d53ccee253777461840e
freestyle385 <freestyle385@gmail.com>
2021-10-19 17:54:49 +0900

    게시물 상세조회 기능(getArticle) 구현_1019

--- src/main/java/com/sbs/exam/demo/controller/UserArticleController.java
+++ src/main/java/com/sbs/exam/demo/controller/UserArticleController.java
@@ -43,7 +43,7 @@ public class UserArticleController {
 		return article;
 	}
 
-	private Article getArticle(int id) {
+	private Article getFoundArticle(int id) {
 		for (Article foundArticle : articles) {
 			if (foundArticle.getId() == id) {
 				return foundArticle;
@@ -53,13 +53,13 @@ public class UserArticleController {
 	}
 
 	private void deleteArticle(int id) {
-		Article foundArticle = getArticle(id);
+		Article foundArticle = getFoundArticle(id);
 
 		articles.remove(foundArticle);
 	}
 
 	private Article modifyArticle(int id, String title, String body) {
-		Article foundArticle = getArticle(id);
+		Article foundArticle = getFoundArticle(id);
 
 		foundArticle.setTitle(title);
 		foundArticle.setBody(body);
@@ -86,7 +86,7 @@ public class UserArticleController {
 	@RequestMapping("/usr/article/doDelete")
 	@ResponseBody
 	public String doDelete(int id) {
-		Article foundArticle = getArticle(id);
+		Article foundArticle = getFoundArticle(id);
 
 		if (foundArticle == null) {
 			return id + "번 글은 존재하지 않습니다.";
@@ -100,7 +100,7 @@ public class UserArticleController {
 	@RequestMapping("/usr/article/doModify")
 	@ResponseBody
 	public String doModify(int id, String title, String body) {
-		Article foundArticle = getArticle(id);
+		Article foundArticle = getFoundArticle(id);
 
 		if (foundArticle == null) {
 			return id + "번 글은 존재하지 않습니다.";
@@ -108,7 +108,19 @@ public class UserArticleController {
 		
 		foundArticle = modifyArticle(id, title, body);
 
-		return id + "번 글이 수정되었습니다.\n" + foundArticle;
+		return id + "번 글이 수정되었습니다.<br>" + foundArticle;
+	}
+	
+	@RequestMapping("/usr/article/getArticle")
+	@ResponseBody
+	public String getArticle(int id) {
+		Article foundArticle = getFoundArticle(id);
+
+		if (foundArticle == null) {
+			return id + "번 글은 존재하지 않습니다.";
+		}
+
+		return "게시물 번호 : " + foundArticle.getId() + "<br>게시물 제목 : " + foundArticle.getTitle() + "<br>게시물 내용 : " + foundArticle.getBody();
 	}
 	// 액션 메소드 끝
 }
