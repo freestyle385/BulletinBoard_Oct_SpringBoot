9a35b9ea32283884b4e8be7019b2b30329c11cbc
freestyle385 <freestyle385@gmail.com>
2021-11-03 16:42:45 +0900

    article 테이블에 작성자 정보(memberId) 컬럼 추가_1103

--- db/DbSchema.sql
+++ db/DbSchema.sql
@@ -79,8 +79,18 @@ nickname = '도라에몽',
 cellPhoneNo = '01011112222',
 email = '123@gmail.com';
 
+# 게시물 테이블에 회원 정보 추가
+ALTER TABLE article ADD COLUMN memberId INT(10) UNSIGNED NOT NULL AFTER updateDate;
+
+# 기존 게시물의 작성자를 2번으로 수정
+UPDATE article SET memberId = 2 WHERE memberId = 0; 
+
 SELECT * 
 FROM article; 
 
 SELECT * 
 FROM `member`;
+
+SELECT COUNT(*) > 0
+FROM `member`
+WHERE loginId = 'user1' AND email = abc@gmail.com;
