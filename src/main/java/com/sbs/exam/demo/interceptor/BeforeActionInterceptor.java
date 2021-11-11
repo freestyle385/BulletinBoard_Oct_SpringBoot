3c88417eec92fdd25e4087a9a8ce77c25b3a4ec4
freestyle385 <freestyle385@gmail.com>
2021-11-11 18:29:09 +0900

    BeforeAction 인터셉터 생성_1111

---
+++ src/main/java/com/sbs/exam/demo/interceptor/BeforeActionInterceptor.java
@@ -0,0 +1,16 @@
+package com.sbs.exam.demo.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class BeforeActionInterceptor implements HandlerInterceptor{
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		
+		// 추후 구현 예정
+		return HandlerInterceptor.super.preHandle(request, response, handler);
+	}
+}
